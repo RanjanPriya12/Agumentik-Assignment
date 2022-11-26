50885202b6ab1acc2644aec386113537949605c9
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2022-11-26 13:03:03 +0530

    file structure created

---
+++ app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Navbar = () => {
+  return (
+    <div className='mainCOntainer'>
+        <ul className='navContainer'>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+  )
+}
